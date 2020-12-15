3e97cb225857940ae9e5a46c88c4cd37301d47e2
Byankina <70693491+Byankina@users.noreply.github.com>
2020-12-15 22:28:21 +0300

    Добавлена менюшка

--- sort_race/sort_race.cpp
+++ sort_race/sort_race.cpp
@@ -4,10 +4,13 @@
 #include <array>
 #include <vector>
 #include <string>
+#include <iostream>
+#include "utils.h" 
 
 using namespace std;
 
 vector<int> GenerateData(int size, int max_value = INT_MAX);
+vector<int> read_data();
 using Participant = std::vector<int>(*)(std::vector<int>);
 void Run(string method_name, Participant p, vector<int> data);
 
@@ -18,14 +21,77 @@ void Run(string method_name, Participant p, vector<int> data);
 
 vector<int> std_sort(vector<int>);
 vector<int> merge_sort_by_danilova(vector<int> data);
+vector<int> Sort_by_Byankina(vector<int>);
+
 
 int main()
 {
-    const array<int, 4> N = { 10, 1'000, 10'000, 1'000'000 };
-   for (int n : N)
-   {
-       auto data = GenerateData(n);
-       RUN(std_sort);
-       RUN(merge_sort_by_danilova);
-   }
+    while (1)
+    {
+        welcome();
+        int user_point = user_choise("Select data source: ");
+        const array<int, 4> N = { 10, 1'000, 10'000, 1'000'000 };
+        switch (user_point) {
+
+        case 1: {
+            for (int n : N) {
+
+                auto data = GenerateData(n, 2);
+                RUN(std_sort);
+                RUN(merge_sort_by_danilova);
+                RUN(Sort_by_Byankina);
+            }
+            break;
+        }
+
+        case 2: {
+
+            for (int n : N) {
+
+                auto data = GenerateData(n, 1);
+                RUN(std_sort);
+                RUN(merge_sort_by_danilova);
+                RUN(Sort_by_Byankina);
+            }
+            break;
+        }
+        case 3: {
+
+            for (int n : N) {
+
+                //auto data = GenerateData_doubles(n);
+                //RUN_Doubles(std_sort_double);
+                //RUN_Doubles(Sort_by_Byankina_d);
+
+            }
+            break;
+        }
+        case 4: {
+
+            auto data = read_data();
+            RUN(std_sort);
+            RUN(merge_sort_by_danilova);
+            RUN(Sort_by_Byankina);
+
+            break;
+        }
+        case 0:
+
+            return 0;
+            break;
+
+        }
+        cout << endl;
+    }
 }
+
+//int main()
+//{
+//    const array<int, 4> N = { 10, 1'000, 10'000, 1'000'000 };
+//   for (int n : N)
+//   {
+//       auto data = GenerateData(n);
+//       RUN(std_sort);
+//       RUN(merge_sort_by_danilova);
+//   }
+//}
--- sort_race/test_run.cpp
+++ sort_race/test_run.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include<fstream>
 
 using namespace std;
 
@@ -25,5 +26,22 @@ vector<int> GenerateData(int size, int max_value = INT_MAX)
 	vector<int> data(size);
 	for (int& d : data)
 		d = rand() % max_value;
+	return data;
+}
+vector<int> read_data()
+{
+
+	vector <int> data;
+
+	int current;
+
+	ifstream in("input.txt");
+
+	if (in.is_open()) {
+		while (in >> current) {
+			data.push_back(current);
+		}
+	}
+
 	return data;
 }
