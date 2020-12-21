d67e02c9eec9ab6b06e1d655765dea1699a01013
igoshkina7 <71019642+igoshkina7@users.noreply.github.com>
2020-12-21 03:21:46 +0300

    Create Choice_Sort_by_Igoshkina.cpp

---
+++ sort_race/Choice_Sort_by_Igoshkina.cpp
@@ -0,0 +1,40 @@
+﻿#include<vector>
+using namespace std;
+
+template <typename T>
+void ChoiceSort_by_Igoshkina(vector<T>& vec, int size)// сортировка выбором
+{
+    for (int repeat_counter = 0; repeat_counter < size; repeat_counter++)  //
+    {
+        int temp = vec[0]; // временная переменная для хранения значения перестановки
+        for (int element_counter = repeat_counter + 1; element_counter < size; element_counter++)
+        {
+            if (vec[repeat_counter] > vec[element_counter])  // находим минимальный элемент, меняем его местами с первым элементом
+            {
+                temp = vec[repeat_counter];
+                vec[repeat_counter] = vec[element_counter];
+                vec[element_counter] = temp;
+            }
+        }
+    }
+}
+
+//template <typename T>
+vector<int>Sort_by_Igoshkina(vector<int>data)
+{
+    if (!data.empty()) {
+        int n = data.size();
+        ChoiceSort_by_Igoshkina(data, n);
+    }
+    return data;
+
+}
+vector<double>Sort_by_Igoshkina(vector<double>data)
+{
+    if (!data.empty()) {
+        int n = data.size();
+        ChoiceSort_by_Igoshkina(data, n);
+    }
+    return data;
+
+}
